fe38577171ac742017a4214bfe771982c42cf1f6
Le Gia Bao <lgbao1062003@gmail.com>
2023-09-27 21:33:48 +0800

    add url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://baole106.github.io/ExpenseTracker/',
+    'baole.expensetracker.io',
     'http://localhost:5000',
     'http://localhost:8000',
     'http://localhost:3000',
